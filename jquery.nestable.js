e6326e2da9d7818aee78cc69cc5d3855c798e7c7
Поляков Илья <polyakov@aspect-systems.ru>
2015-03-11 19:05:22 +0300

    default max depth increased.

--- jquery.nestable.js
+++ jquery.nestable.js
@@ -40,7 +40,7 @@
         expandBtnHTML: '<button data-action="expand" type="button">Expand</button>',
         collapseBtnHTML: '<button data-action="collapse" type="button">Collapse</button>',
         group: 0,
-        maxDepth: 5,
+        maxDepth: 50,
         threshold: 20
     };
 
